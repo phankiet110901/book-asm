6240477eecce54be94f7eaa5b0fe0c1b6b389ce0
phankiet110901 <phankiet110901@gmail.com>
2021-02-14 01:55:35 +0700

    setup for deloy

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -3,11 +3,11 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: 'ec2-52-7-168-69.compute-1.amazonaws.com',
     port: 5432,
-    username: 'postgres',
-    password: 'Kietpro@123',
-    database: 'book-asm',
+    username: 'cldyhkbgfprxkz',
+    password: '5d1193622a65ef063f5c4bf21225694cf9b482415e54b98e758c9ca06889dddc',
+    database: 'd3pvd2qljh6883',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: false
 }
